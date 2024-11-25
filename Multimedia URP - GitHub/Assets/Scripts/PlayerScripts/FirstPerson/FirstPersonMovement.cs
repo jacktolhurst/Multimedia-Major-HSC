4c97aa18fdef7150207003f8e3e10df71df43766
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2024-11-25 12:31:28 +1100

    Revert "Added first person movement"
    This reverts commit 7a3087b8e79d2c1ae63cc9b0d7345a59ee04f8ab.

--- Multimedia URP - GitHub/Assets/Scripts/PlayerScripts/FirstPerson/FirstPersonMovement.cs
+++ Multimedia URP - GitHub/Assets/Scripts/PlayerScripts/FirstPerson/FirstPersonMovement.cs
@@ -2,26 +2,16 @@ using UnityEngine;
 
 public class FirstPersonMovement : MonoBehaviour
 {
-    private Rigidbody rb;
-
-    private Renderer playerRenderer;
-
-    [SerializeField] private Transform orientation;
+    Rigidbody rb;
 
     private Vector3 moveDirection;
 
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpForce;
-    private float movementMultiplier = 10;
-    [SerializeField] private float jumpMultiplier = 0.4f;
-    private float groundDrag = 6f;
-    private float airBag = 2f;
 
-    private bool isGrounded;
+    [SerializeField] private float speed;
+    [SerializeField] private float drag;
 
     void Awake(){
         rb = GetComponent<Rigidbody>();
-        playerRenderer = GetComponentInChildren<Renderer>();
 
         rb.freezeRotation = true;
 
@@ -29,34 +19,19 @@ public class FirstPersonMovement : MonoBehaviour
     }
 
     void Update(){
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerRenderer.bounds.extents.y + 0.1f);
-
         GetInput();
         ControlDrag();
-
-        if(Input.GetKeyDown("space") && isGrounded){
-            Jump();
-        }
     }
 
     private void GetInput(){
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-    }
-
-    private void Jump(){
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
     }
 
     private void ControlDrag(){ 
-        if(isGrounded){
-            rb.linearDamping = groundDrag;
-        } 
-        else{
-            rb.linearDamping = airBag;
-        }
+        rb.linearDamping = drag;
     }
 
     void FixedUpdate(){
@@ -64,12 +39,6 @@ public class FirstPersonMovement : MonoBehaviour
     }
 
     private void Movement(){
-        if(isGrounded){
-            rb.AddForce(moveDirection.normalized * speed * movementMultiplier, ForceMode.  Acceleration);
-        }
-        else{
-            rb.AddForce(moveDirection.normalized * speed * movementMultiplier * jumpMultiplier, ForceMode.  Acceleration);
-
-        }
+        rb.AddForce(moveDirection.normalized * speed, ForceMode.Acceleration);
     }
 }
