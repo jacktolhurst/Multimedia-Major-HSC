f1251dad621649c4db53c2ef53266581b5cfd9ca
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2025-05-28 23:31:43 +1000

    Simple but not finished fixed for particle movement

--- Multimedia URP - GitHub/Assets/Scripts/Audio/NoteParticle/NoteParticleManager.cs
+++ Multimedia URP - GitHub/Assets/Scripts/Audio/NoteParticle/NoteParticleManager.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class NoteParticleManager : MonoBehaviour
 {
-    [HideInInspector] public Rigidbody followObjRb = null;
+    [HideInInspector] public Rigidbody followObjRb;
     private Rigidbody selfRb;
+
+    private Collider[] followObjColliders;
+    private Collider selfCollider;
+
+    private List<Bounds> followObjBounds = new List<Bounds>();
+    private Bounds selfBound;
 
     private Vector3 targetPos;
     private Vector3 velocity = Vector3.zero;
@@ -14,7 +22,15 @@ public class NoteParticleManager : MonoBehaviour
     [HideInInspector] public float speed = 1;
     private float randScaleChangeTime;
 
+    [HideInInspector] public bool usingGameObject = false;
+
     void Start(){
+        StartCoroutine(AfterStart());
+    }
+
+    private IEnumerator AfterStart(){
+        yield return null;
+
         startTime = Time.time;
         lifeTime = endTime - startTime;
 
@@ -26,6 +42,31 @@ public class NoteParticleManager : MonoBehaviour
 
         selfRb = GetComponent<Rigidbody>();
         selfRb.linearVelocity = (transform.position - targetPos).normalized * speed;
+
+        if(usingGameObject){
+            selfCollider = GetComponent<Collider>();
+            selfCollider.isTrigger = false;
+            followObjColliders = followObjRb.gameObject.GetComponents<Collider>();
+
+            foreach(Collider followObjCollider in followObjColliders) followObjBounds.Add(followObjCollider.bounds);
+
+            selfBound = GetComponent<Collider>().bounds;
+
+            CheckIfColliding();
+        }
+
+        // Bounds bounds = obj.GetComponent<Collider>().bounds;
+        // Vector3 extents = bounds.extents;
+        // List<GameObject> newNoteParticleObjs = new List<GameObject>();
+
+        // Vector3 spawnPoint = Vector3.zero;
+        // if(bounds != null){
+            // Vector3 dir = Random.onUnitSphere;
+            // float requiredDistance =  (Mathf.Abs(dir.x) * extents.x) + (Mathf.Abs(dir.y) * extents.y) + (Mathf.Abs(dir.z) * extents.z);
+            // float spawnDistance = requiredDistance + 1;
+            // spawnPoint = bounds.center + dir * spawnDistance;
+        // }
+        // else spawnPoint = obj.transform.position;
     }
 
     void Update(){
@@ -36,6 +77,18 @@ public class NoteParticleManager : MonoBehaviour
         if(randScaleChangeTime < Time.time){   
             transform.localScale = Vector3.SmoothDamp(transform.localScale, Vector3.zero, ref velocity,0.1f);
         }
-        
+
+        if(usingGameObject) CheckIfColliding();
+    }
+
+    private void CheckIfColliding(){
+        foreach(Bounds followObjBound in followObjBounds){
+            if(selfBound.Intersects(followObjBound)){
+                foreach(Collider followObjCollider in followObjColliders) Physics.IgnoreCollision(selfCollider, followObjCollider, true);
+            }
+            else{
+                foreach(Collider followObjCollider in followObjColliders) Physics.IgnoreCollision(selfCollider, followObjCollider, false);
+            }
+        }
     }
 }
